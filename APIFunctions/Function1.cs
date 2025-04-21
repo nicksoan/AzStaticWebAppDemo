a43c08a3fd165917f885e765d1d7156a93d11fe3
Nick Soan <nick.soan@googlemail.com>
2025-04-21 16:42:48 +0100

    Add project files.

---
+++ APIFunctions/Function1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace APIFunctions
+{
+    public static class Function1
+    {
+        [FunctionName("hello")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "hello")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "Hello unknown user!"
+                : $"Hello, {name}.🙋🏻‍♂️ ";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
---
+++ AzStaticWebAppDemo/wwwroot/js/main.js
@@ -0,0 +1,7 @@
+﻿document.getElementById('ping').onclick = async () => {
+    console.log('Ping button clicked');
+    //const res = await fetch('/api/hello');
+    const res = await fetch('http://localhost:7123/api/hello');
+    const text = await res.text();
+    document.getElementById('out').textContent = text;
+};
